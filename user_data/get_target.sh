84600dbfa6e4a9d8fb89423a83fb4b8c29591ac2
YuriiVK <you@example.com>
2022-03-03 21:44:40 +0200

    put user_data to script file

--- user_data/get_target.sh
+++ user_data/get_target.sh
@@ -6,16 +6,16 @@ declare -a TARGETS
 while [ "$SEM" != "$(hostname)" ]; do
   while [ "$SEM" != "available" ]; do
     sleep $R
-    SEM=$(aws ssm get-parameter --name "/dns/sem" --region $REGION|jq -r .Parameter.Value)
+    SEM=$(aws ssm get-parameter --name "/$PREFIX/sem" --region $REGION|jq -r .Parameter.Value)
   done
   #set parameter write lock in the semaphore
-  aws ssm put-parameter --name "/dns/sem" --overwrite --region $REGION --value $(hostname)
+  aws ssm put-parameter --name "/$PREFIX/sem" --overwrite --region $REGION --value $(hostname)
   sleep $R
   #check if lock successful
-  SEM=$(aws ssm get-parameter --name "/dns/sem" --region $REGION|jq -r .Parameter.Value)
+  SEM=$(aws ssm get-parameter --name "/$PREFIX/sem" --region $REGION|jq -r .Parameter.Value)
 done
 #get first element as TARGET and shift it to the end of array
-TARGETS=($(aws ssm get-parameter --name "/dns/targets" --region $REGION|jq -r .Parameter.Value))
+TARGETS=($(aws ssm get-parameter --name "/$PREFIX/targets" --region $REGION|jq -r .Parameter.Value))
 #get first TARGET from SSM parameter and rotate massive
 TARGET=${TARGETS[0]}
 unset TARGETS[0]
@@ -23,6 +23,8 @@ TARGETS+=($TARGET)
 echo ------------TARGET=$TARGET-------------------
 #update rotated TARGETS in the SSM parameter
 T=$(printf "%s\n" "${TARGETS[@]}")
-aws ssm put-parameter --name "/dns/targets" --overwrite --region $REGION --value "$T"
+aws ssm put-parameter --name "/$PREFIX/targets" --overwrite --region $REGION --value "$T"
 #release lock
-aws ssm put-parameter --name "/dns/sem" --overwrite --region $REGION --value "available"
+aws ssm put-parameter --name "/$PREFIX/sem" --overwrite --region $REGION --value "available"
+#set environment variable TARGET
+echo TARGET=$TARGET>>/etc/environment
